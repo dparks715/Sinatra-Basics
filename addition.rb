1d3042e3e34f30cb2b8984a8ee5e3ba31980dd91
Daniel Parks <dparks715@yahoo.com>
2017-02-18 01:33:42 -0500

    Function tested and works, moving to app and erb

---
+++ addition.rb
@@ -0,0 +1,7 @@
+#Adds 3 inputs
+
+def add(num1, num2, num3)
+	sum = num1 + num2 + num3
+	sum
+	
+end
--- test_addition.rb
+++ test_addition.rb
@@ -6,4 +6,19 @@ class TestAddition < Minitest::Test
 		assert_equal(1, 1)	
 	end
 
+	def test_one_one_one
+		results = add(1, 1, 1)
+		assert_equal(3, results)	
+	end
+
+	def test_one_two_three
+		results = add(1, 2, 3)
+		assert_equal(6, results)	
+	end
+
+	def test_four_ten_nineteen
+		results = add(4, 10, 19)
+		assert_equal(33, results)	
+	end
+
 end
