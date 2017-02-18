e787869b6022d54de550fbc014d47880c991a613
Daniel Parks <dparks715@yahoo.com>
2017-02-18 02:23:09 -0500

    Added erb for fav numbers, modified app to match

--- app.rb
+++ app.rb
@@ -2,9 +2,18 @@ require 'sinatra'
 require_relative 'addition.rb'
 
 get '/' do
-	erb :add
+	erb :add, :locals => {name_input: ''}
 end
 
 post '/addition_form' do
 	user_name = params[:name_input]
+
+	erb :fav_numbers, :locals => {user_name: user_name}
+end
+
+post '/favorite_nums' do
+	num1 = params[:num1]
+	num2 = params[:num2]
+	num3 = params[:num3]
+
 end
