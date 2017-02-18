b50de1663cd3f25b3ff4173415eb4aa10f84565f
Daniel Parks <dparks715@yahoo.com>
2017-02-18 08:35:29 -0500

    App.rb almost done, working on last erb

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ post '/addition_form' do
 end
 
 post '/age_form' do
-	user_age = params[:age_input]
+	user_age = params[:age_input].to_i
 	user_name = params[:name_input]
 
 
@@ -21,8 +21,24 @@ post '/age_form' do
 end
 
 post '/favorite_nums' do
-	num1 = params[:num1]
-	num2 = params[:num2]
-	num3 = params[:num3]
+	user_age = params[:age_input]
+	user_name = params[:name_input]
+	num1 = params[:num1].to_i
+	num2 = params[:num2].to_i
+	num3 = params[:num3].to_i
+
+	erb :total, :locals => {
+		user_age: user_age,
+		user_name: user_name, 
+		num1: num1,
+		num2: num2,
+		num3: num3
+	}
 
 end
+
+post '/total' do
+	results = add(num1, num2, num3)
+	erb :total, :locals => {results: results}
+end
+
